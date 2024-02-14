65567d7c2c93408ef55128d63ae2303fbb83be01
ronaldof16 <ronaldof16@hotmail.com>
2024-02-14 12:20:25 -0300

    importação do componente com a tela de categoria do Quiz

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,10 @@ import { QuizContext } from './context/quiz';
 import Welcome from './components/Welcome';
 import Question from './components/Question';
 import GameOver from './components/GameOver';
+import PickCategory from './components/PickCategory';
 
 import './App.css';
+
 
 function App() {
   const [quizState, dispatch] = useContext(QuizContext);
@@ -20,6 +22,7 @@ function App() {
     <div className='App'>
       <h1>Quiz de Programação</h1>
       {quizState.gameStage === 'Start' && <Welcome />}
+      {quizState.gameStage === 'Category' && <PickCategory />}
       {quizState.gameStage === 'Playing' && <Question />}
       {quizState.gameStage === 'End' && <GameOver />}
     </div>
