1393909c45c269330745f3c02ecb8ab4f063d525
ronaldof16 <ronaldof16@hotmail.com>
2024-02-18 22:27:26 -0300

    criação do type 'START_GAME' para reordenar as questões só depois da escolha da categoria

--- src/context/quiz.jsx
+++ src/context/quiz.jsx
@@ -1,7 +1,7 @@
 import { createContext, useReducer } from "react";
-import questions from '../data/questions';
+import questions from '../data/questions_complete';
 
-const STAGES = ['Start', 'Playing', 'End'];
+const STAGES = ['Start', 'Category', 'Playing', 'End'];
 
 const initialState = {
     gameStage: STAGES[0],
@@ -20,8 +20,23 @@ const quizReducer = (state, action) => {
                 gameStage: STAGES[1]
             };
 
+        case 'START_GAME':
+            let quizQuestions = null;
+
+            state.questions.forEach((question) => {
+                if(question.category === action.payload) {
+                    quizQuestions = question.questions;
+                }
+            })
+
+            return {
+                ...state,
+                questions: quizQuestions,
+                gameStage: STAGES[2]
+            }
+
         case 'REORDER_QUESTIONS':
-            const reorderedQuestions = questions.sort(() => {
+            const reorderedQuestions = state.questions.sort(() => {
                 return Math.random() - 0.5;
             });
 
@@ -34,14 +49,14 @@ const quizReducer = (state, action) => {
             const nextQuestion = state.currentQuestion + 1;
             let endGame = false;
             
-            if(!questions[nextQuestion]) {
+            if(!state.questions[nextQuestion]) {
                 endGame = true;
             }
 
             return {
                 ...state,
                 currentQuestion: nextQuestion,
-                gameStage: endGame ? STAGES[2] : state.gameStage,
+                gameStage: endGame ? STAGES[3] : state.gameStage,
                 answerSelected: false
             }
 
