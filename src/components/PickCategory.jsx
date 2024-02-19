73ff28987037ff118b229696b314d929c0b93848
ronaldof16 <ronaldof16@hotmail.com>
2024-02-18 22:24:19 -0300

    criação da função para escolher categoria e reordenar questões

--- src/components/PickCategory.jsx
+++ src/components/PickCategory.jsx
@@ -5,14 +5,22 @@ import './PickCategory.css';
 import Category from '../img/category.svg';
 
 const PickCategory = () => {
-    
+  const [quizState, dispatch] = useContext(QuizContext);
+
+  const chooseCategoryAndReorderQuestions = (category) => {
+    dispatch({ type: 'START_GAME', payload: category });
+
+    dispatch({ type: 'REORDER_QUESTIONS' });
+  }
 
   return (
     <div id='category'>
         <h2>Escolha uma categoria</h2>
         <p>As perguntas serão referentes a uma das linguagens abaixo</p>
         <div>
-            <button>CSS</button>
+            {quizState.questions.map((question) => (
+              <button onClick={() => chooseCategoryAndReorderQuestions(question.category)} key={question.category}>{question.category}</button>
+            ))}
         </div>
         <img src={Category} alt="Categorias do Quiz" />
     </div>
