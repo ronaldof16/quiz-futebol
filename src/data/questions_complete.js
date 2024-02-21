1074c79de8d4e9191c5cb0c9521bd28ccde49cd7
ronaldof16 <ronaldof16@hotmail.com>
2024-02-21 10:53:37 -0300

    criação das categorias times brasileiros e times Internacionais

--- src/data/questions_complete.js
+++ src/data/questions_complete.js
@@ -1,55 +1,78 @@
 const data = [
     {
-      category: "HTML",
+      category: "Times Brasileiros",
       questions: [
         {
-          question: "Qual tag cria um parágrafo?",
-          options: ["<p>", "<h1>", "<text>", "<ul>"],
-          answer: "<p>",
-          tip: "É uma tag de uma letra apenas",
+          question: "Qual time tem a maior torcida do Brasil?",
+          options: ["São Paulo", "Corinthians", "Flamengo", "Palmeiras"],
+          answer: "Flamengo",
+          tip: "Time de coração do ex-jogador zico",
         },
         {
-          question: "Qual atributo adiciona um link para a tag a?",
-          options: ["alt", "href", "src", "link"],
-          answer: "href",
-          tip: "Hyperlink Reference",
+          question: "Qual time tem mais títulos do mundial de clubes da FIFA?",
+          options: ["Flamengo", "Cruzeiro", "Santos", "São Paulo"],
+          answer: "São Paulo",
+          tip: "É um time Paulista",
         },
         {
-          question: "As listas não ordenadas tem a tag de:",
-          options: ["<ol>", "<ul>", "<li>", "<list>"],
-          answer: "<ul>",
+          question: "O maior campeão da copa do Brasil é o:",
+          options: ["Cruzeiro", "Grêmio", "Fluminense", "Palmeiras"],
+          answer: "Cruzeiro",
+          tip: "Time que Ronaldo fenômeno comprou"
         },
         {
-          question: "Qual atributo deixa o input obrigatório?",
-          options: ["placeholder", "value", "required", "maxlength"],
-          answer: "required",
+          question: "Qual time nunca foi rebaixado para a série B do campeonato brasileiro?",
+          options: ["Internacional", "Vasco", "Corinthians", "São Paulo"],
+          answer: "São Paulo",
+          tip: "Tem o maior estádio particular do Brasil"
         },
         {
-          question: "A tag semântica indicada para rodapés é a:",
-          options: ["div", "main", "section", "footer"],
-          answer: "footer",
+          question: "O time que mais participou da taça Libertadores da América é o:",
+          options: ["Palmeiras", "São Paulo", "Grêmio", "Flamengo"],
+          answer: "Palmeiras",
+          tip: "Sua camisa é verde"
         },
       ],
     },
     {
-      category: "CSS",
+      category: "Times Internacionais",
       questions: [
+        /*{
+          question: "Qual o maior campeão da Champions League?",
+          options: ["Barcelona", "Bayern de Munique", "Milan", "Real Madrid"],
+          answer: "Real Madrid",
+          tip: "Time que já foi chamado de Galáticos",
+        },*/
         {
-          question: "Qual regra altera a cor de um elemento?",
-          options: ["color", "background-color", "font-size", "transition"],
-          answer: "color",
-          tip: "Cor em inglês",
+          question: "Qual time da Argentina tem mais títulos nacionais?",
+          options: ["Boca Juniors", "River Plate", "Independiente", "Estudiantes"],
+          answer: "River Plate",
+          tip: "Monumental de Núñez é o nome do seu estádio"
         },
         {
-          question: "Para aumentar a fonte de um elemento utilizamos:",
-          options: ["font", "text-transform", "font-size", "hover"],
-          answer: "font-size",
+          question: "Em que time Cristiano Ronaldo não jogou?:",
+          options: ["Manchester United", "Manchester City", "Sporting", "Juventus"],
+          answer: "Manchester City",
+          tip: "É um time da Inglaterra"
         },
         {
-          question: "A posição que deixa um elemento fixo é a:",
-          options: ["static", "absolute", "fixed", "relative"],
-          answer: "fixed",
+          question: "Qual o time que Ronaldinho Gaúcho jogava antes de ir para o Barcelona?",
+          options: ["PSG", "Ajax", "Lyon", "PSV"],
+          answer: "PSG",
+          tip: "Tem sua sede em Paris"
         },
+        {
+          question: "Para qual time Neymar foi quando deixou o Santos?",
+          options: ["Real Madrid", "PSG", "Barcelona", "Chelsea"],
+          answer: "Barcelona",
+          tip: "Time que Messi mais jogou"
+        },
+        {
+          question: "Em qual time italiano Adriano imperador fez sucesso?",
+          options: ["Roma", "Juventus", "Inter de Milão", "Milan"],
+          answer: "Inter de Milão",
+          tip: "É sediado na cidade de Milão"
+        }
       ],
     },
     {
