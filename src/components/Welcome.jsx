ece01a5a0068147acdb834dff122ca83dcbcd5c1
ronaldof16 <ronaldof16@hotmail.com>
2024-02-24 15:05:36 -0300

    alteração no texto do parágrafo

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -2,6 +2,7 @@ import { useContext } from 'react';
 import { QuizContext } from '../context/quiz';
 
 import './Welcome.css';
+import Quiz from '../img/quiz.svg';
 
 const Welcome = () => {
   const [quizState, dispatch] = useContext(QuizContext);
@@ -11,10 +12,11 @@ const Welcome = () => {
   return (
     <div id='welcome'>
         <h1>Quiz de Futebol</h1>
-        <h2>Seja bem vindo ao nosso quiz, teste seus conhecimentos e aproveite para 
-        aprender ainda mais sobre esse esporte que é uma paixão nacional! </h2>
+        <p>Seja bem vindo ao nosso quiz, teste seus conhecimentos e aproveite para 
+        aprender ainda mais sobre esse esporte que é uma paixão Mundial!</p>
         <p>Clique no botão abaixo para começar</p>
         <button onClick={() => dispatch({type: 'CHANGE_STATE'})}>Iniciar</button>
+        <img src={Quiz} alt="Imagem do Quiz" />
     </div>
   )
 }
