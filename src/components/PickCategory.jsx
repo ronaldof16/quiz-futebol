bb636f1269c97c0b241e07a2ddfc485840fff13d
ronaldof16 <ronaldof16@hotmail.com>
2024-02-24 15:04:13 -0300

    criação de um span com um ícone em cada informação

--- src/components/PickCategory.jsx
+++ src/components/PickCategory.jsx
@@ -2,7 +2,6 @@ import { useContext } from 'react';
 import { QuizContext } from '../context/quiz';
 
 import './PickCategory.css';
-import Category from '../img/category.svg';
 
 const PickCategory = () => {
   const [quizState, dispatch] = useContext(QuizContext);
@@ -15,16 +14,24 @@ const PickCategory = () => {
 
   return (
     <div id='category'>
-        <h2>Escolha uma categoria</h2>
-        <p>As perguntas serão referentes a uma das linguagens abaixo</p>
-        <div>
-            {quizState.questions.map((question) => (
-              <button onClick={() => chooseCategoryAndReorderQuestions(question.category)} key={question.category}>{question.category}</button>
-            ))}
-        </div>
-        <img src={Category} alt="Categorias do Quiz" />
+      <h1>Quiz de Futebol</h1>
+      <div className='informacoes'>
+        <h2>Informações</h2>
+        <ul>
+          <li className='informacoes-itens'><span>✔︎</span> Cada tópico é composto por cinco perguntas, e cada pergunta possui apenas uma alternativa verdadeira.</li>
+          <li className='informacoes-itens'><span>✔︎</span> Em todas as perguntas você vai ter a opção de obter uma dica sobre a resposta correta ou excluir uma alternativa errada.</li>
+          <li className='informacoes-itens'><span>✔︎</span> Ao selecionar uma alternativa não será possível fazer uma mudança para outra, e no mesmo momento o usuário vai saber se 
+          acertou ou não a questão através da cor de fundo da alternativa: VERDE - CORRETA  e VERMELHO - ERRADA.</li>
+        </ul>
+      </div>
+      <h2>Escolha um tópico abaixo e comece a responder as perguntas</h2>
+      <div className='topicos'>
+        {quizState.questions.map((question) => (
+          <button onClick={() => chooseCategoryAndReorderQuestions(question.category)} key={question.category}>{question.category}</button>
+        ))}
+      </div>
     </div>
   )
 }
 
-export default PickCategory
+export default PickCategory;
